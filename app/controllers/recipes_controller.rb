260bf2354b1659f1f0ef9bdca8870da18d18e6f2
vashjun <vashajunot@gmail.com>
2015-09-05 07:44:40 -0400

    Added edit/update functionality to Recipes, fixed format in description

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,7 +23,20 @@ class RecipesController < ApplicationController
     else
       render :new
     end
-    
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Your recipe was updated successfully."
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
   
   private
