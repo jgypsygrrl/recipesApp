b366e51762a6968e4f6374aea56f5951cb5e851c
vashjun <vashajunot@gmail.com>
2015-09-22 23:21:50 -0400

    recipesApp: Add style and ingredient tables, minor edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910060858) do
+ActiveRecord::Schema.define(version: 20150923031009) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20150910060858) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -37,6 +41,10 @@ ActiveRecord::Schema.define(version: 20150910060858) do
     t.datetime "updated_at"
     t.integer  "chef_id"
     t.string   "picture"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
   end
 
 end
