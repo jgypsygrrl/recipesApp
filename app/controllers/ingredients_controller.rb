8f1dd75e96099a972985f25ea053daec41cc3ef9
vashjun <vashajunot@gmail.com>
2015-09-26 22:13:57 -0400

    recipeApp: Create show pages for ingredients and styles

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,10 @@
 class IngredientsController < ApplicationController
 
+  def show
+    @ingredient = Ingredient.find(params[:id])
+    @recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
   def new
     @ingredient = Ingredient.new
   end
@@ -12,6 +17,10 @@ class IngredientsController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def show
+    @ingredient = Ingredient.find(params[:id])
   end
   
   private
