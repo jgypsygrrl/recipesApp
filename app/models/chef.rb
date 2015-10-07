7a23ec94fd0b546c414726241787816d8b139b20
vashjun <vashajunot@gmail.com>
2015-10-06 23:26:02 -0400

    recipeApp: Add bios for chefs

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -3,6 +3,7 @@ class Chef < ActiveRecord::Base
   has_many :likes
   before_save { self.email = email.downcase }
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
+  validates :bio, presence: true, length: { minimum: 3, maximum: 200 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 105 },
                                     uniqueness: { case_sensitive: false },
